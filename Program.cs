86ea2bf7009b914526ee0c77a6a9dd0f1f3360d3
Prajwal V <prajwalvishwamurthy@gmail.com>
2022-04-18 17:02:01 +0530

    DotNet

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace netpractisce
+{
+    class Program
+    {
+    static void Main(string[] args)
+    {
+        List<String> Str1 =new List<string>();
+        Str1.Add("prajwal.v");
+        Str1.Add("Puneeth Rajkumar");
+        Console.WriteLine("Hello World!");
+
+        
+
+        foreach (var str in Str1)
+        {
+            Console.WriteLine(Str1);
+        }
+    }
+    }
+}
